9bd74e9919e6e253bdf6773a7824654b0409b3b8
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2024-05-02 11:36:38 +0530

    added dark ode functionality

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,7 +10,7 @@ import {
 } from 'react-icons/bs';
 const FooterCom = () => {
   return (
-    <Footer container className="border border-t-4">
+    <Footer container className="border border-t-4 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </Provider>
     </PersistGate>
   </React.StrictMode>
