c5e391e77af58332a1b662b6dce3860d72dd827e
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2024-05-23 19:43:37 +0530

    added create update and delete functionality

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -5,5 +5,15 @@ const postController = require('../controllers/postController.js');
 const router = express.Router();
 
 router.post('/create', verifyToken, postController.create);
-
+router.get('/getposts', postController.getPosts);
+router.delete(
+  '/deletepost/:postId/:userId',
+  verifyToken,
+  postController.deletepost
+);
+router.put(
+  '/updatepost/:postId/:userId',
+  verifyToken,
+  postController.updatepost
+);
 exports.router = router;
